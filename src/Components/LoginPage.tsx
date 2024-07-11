f219f259f9a8c76b1044186a624cad655279ae32
hosnidotech123 <hosnidotech@gmail.com>
2024-07-11 11:36:39 +0100

    ppppt

--- src/Components/LoginPage.tsx
+++ src/Components/LoginPage.tsx
@@ -11,7 +11,7 @@ function LoginPage() {
           <div className="h-[500px] w-[400px] bg-purple-800 rounded-xl flex flex-col justify-center items-center">
             <span className='text-white text-[90px]'><MdAdminPanelSettings /></span>
             <h1 className="font-bold text-white mb-7 text-[40px]">Log in</h1>
-            <input required className="m-2 h-10 rounded-md p-2 bg-white text-black" type="text" placeholder="Email"/>
+            <input required className="m-2 h-10 rounded-md p-2 bg-white text-black" type="text" placeholder="Username or Email"/>
               <input required className="m-2 mb-8 h-10 rounded-md p-2 bg-white text-black" type="password" placeholder="Password"/>
                 <button className="text-white bg-blue-700 w-[200px] rounded-md h-10" type="submit">Log in</button>
               </div>
